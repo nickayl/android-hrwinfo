9b24082380bc4600d2f00d2545e49f88944f5722
Domenico <dom.aiello90@gmail.com>
2018-12-15 19:30:14 +0100

    Cleaned working directory - removed non-project files

--- app/src/main/java/com/javando/hrwinfo/MainActivity.java
+++ app/src/main/java/com/javando/hrwinfo/MainActivity.java
@@ -1,105 +1,35 @@
 package com.javando.hrwinfo;
 
-import android.annotation.SuppressLint;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import org.javando.android.hrwinfo.core.api.AndroidHrwInfo;
+import org.javando.android.hrwinfo.core.api.CPU;
+import org.javando.android.hrwinfo.core.api.Device;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Debug";
-    public static final String CONTENT = "content";
-    private Button button;
-    private TextView textView;
-    private EditText editText;
 
-    private int counter = 0;
-
-    @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "== onCreate: == called");
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Device device = AndroidHrwInfo.getInstance().device(this);
+        Log.d(TAG, "onCreate: "+device.toString());
 
-        button = findViewById(R.id.button);
-        textView = findViewById(R.id.textView);
 
-        if(savedInstanceState != null && savedInstanceState.get(CONTENT) != null) {
-            textView.setText(savedInstanceState.get(CONTENT).toString());
-        }
+        AndroidHrwInfo.getInstance()
+                .cpu()
+                .setOnFrequencyChangeListener(cores -> {
+                    String str = "[";
+                    for(CPU.Core core: cores) {
+                        str += String.format("#%d %d ",core.getCoreNumber(), core.getCurFrequency());
+                    }
+                    str += "] Mhz\n";
+                    Log.d(TAG, str);
+                })
+                .startCpuFrequencyMonitor(1000);
 
-        textView.setMovementMethod(new ScrollingMovementMethod());
-
-        button.setOnClickListener(v -> {
-            textView.append(
-                    String.format("The button counter has been tapped %d times\n", counter++)
-            );
-        });
+        Log.d(TAG, "onCreate: ciaooooo");
     }
-
-    @Override
-    protected void onStart() {
-        Log.d(TAG, "onStart: ___started___");
-        super.onStart();
-    }
-
-
-     @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-         Log.d(TAG, "onRestoreInstanceState: ||| called |||");
-         String content = savedInstanceState.getString(CONTENT);
-         if(content != null) {
-             Log.d(TAG, "onRestoreInstanceState: setting content in textview!");
-             textView.setText(content);
-         }
-         super.onRestoreInstanceState(savedInstanceState);
-     }
-
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        Log.d(TAG, "onSaveInstanceState: ììì called ììì");
-        super.onSaveInstanceState(outState);
-        outState.putString(CONTENT, textView.getText().toString());
-    }
-
-    @Override
-    protected void onResume() {
-        Log.d(TAG, "onResume: ***called ***");
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        Log.d(TAG, "onPause:/// called ///");
-        super.onPause();
-    }
-
-    @Override
-    protected void onRestart() {
-        Log.d(TAG, "onRestart: ---called---");
-        super.onRestart();
-    }
-
-    @Override
-    protected void onStop() {
-        Log.d(TAG, "onStop: +++called+++");
-        super.onStop();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        Log.d("Debug", "Configuration changed: "+newConfig.orientation);
-        super.onConfigurationChanged(newConfig);
-
-    }
-
-
 }
