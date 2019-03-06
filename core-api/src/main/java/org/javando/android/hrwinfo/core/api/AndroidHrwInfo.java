fcaba2118b25e1304c566b88c2a396bd951a55ae
Domenico <dom.aiello90@gmail.com>
2019-03-06 14:22:57 +0100

    Improved singleton instatiation in AndroidhrwInfo

--- core-api/src/main/java/org/javando/android/hrwinfo/core/api/AndroidHrwInfo.java
+++ core-api/src/main/java/org/javando/android/hrwinfo/core/api/AndroidHrwInfo.java
@@ -8,16 +8,24 @@ public abstract class AndroidHrwInfo {
 
     private static AndroidHrwInfo instance;
 
-    public static AndroidHrwInfo getInstance() {
-        if(instance == null) {
+    static {
 
-            try {
-                Class klass = Class.forName("org.javando.android.hrwinfo.core.impl.AndroidHrwInfoImpl");
-                instance = (AndroidHrwInfo) klass.newInstance();
-            } catch (Exception e) {
-                throw new RuntimeException("Class AndroidHrwInfoImpl not found. Maybe you've forgotten to add an implementation dependency for this api?");
-            }
+        try {
+            Class klass = Class.forName("org.javando.android.hrwinfo.core.impl.AndroidHrwInfoImpl");
+            instance = (AndroidHrwInfo) klass.newInstance();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Class AndroidHrwInfoImpl not found. Maybe you've forgotten to add an implementation dependency for this api?");
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            throw new RuntimeException("The provided AndroidHrwInfoImpl class does not have a public constructor, therefore cannot be instantiated. " +
+                    "This is a sympthom of a malformed implementation API.");
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Something gone wrong instantiating the AndroidHrwInfoImpl class. The cause is likely to be a malformed implementation API.");
         }
+    }
+
+    public static AndroidHrwInfo getInstance() {
         return instance;
     }
 
